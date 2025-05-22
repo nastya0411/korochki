3307c9c41e1c3ce79fe5a005bd358d00485a86f6
evghn <evghn94_git@mail.ru>
2025-05-23 01:32:11 +0300

    change config conetion db

--- config/db.php
+++ config/db.php
@@ -1,5 +1,21 @@
 <?php
 
+
+if (str_contains(__DIR__, "home")) {
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=MariaDB-11.2;dbname=korochki',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+
+        // Schema cache options (for production environment)
+        //'enableSchemaCache' => true,
+        //'schemaCacheDuration' => 60,
+        //'schemaCache' => 'cache',
+    ];
+}
+
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=korki',
